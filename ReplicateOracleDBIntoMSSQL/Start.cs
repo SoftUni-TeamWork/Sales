7ba7a9dd04967d2408f3406dee54feadfb042a90
Nina Markova <ninamarkoff@users.noreply.github.com>
2015-03-17 18:38:08 +0200

    Console messages added when starting and finishing the data migration;

--- ReplicateOracleDBIntoMSSQL/Start.cs
+++ ReplicateOracleDBIntoMSSQL/Start.cs
@@ -1,4 +1,6 @@
-﻿namespace ReplicateOracleDBIntoMSSQL
+﻿using System;
+
+namespace ReplicateOracleDBIntoMSSQL
 {
     using SQLModelCodeFirst;
     
@@ -6,9 +8,9 @@
     {
         public static void Main()
         {
-
+            Console.WriteLine("Start of data migration from Oracle to MS SQL, please wait :-)");
             Migrations.Configuration.SynchronizeSQLDb(new SQLEntities());
-
+            Console.WriteLine("Successful data migration!");
         }
     }
 }
