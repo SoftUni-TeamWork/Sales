640997726be78dbfdd3764af71f6585794dfa86f
Nina Markova <ninamarkoff@users.noreply.github.com>
2015-03-18 20:33:09 +0200

    Edited entities in Oracle sync in SQL added;

--- ReplicateOracleDBIntoMSSQL/Migrations/Configuration.cs
+++ ReplicateOracleDBIntoMSSQL/Migrations/Configuration.cs
@@ -36,13 +36,21 @@ namespace ReplicateOracleDBIntoMSSQL.Migrations
             var measurmentUnits = db.MEASURMENT_UNITS.ToList();
             foreach (var measurmentUnit in measurmentUnits)
             {
-                if (!(context.MeasurmentUnits.Any(p => p.Id == measurmentUnit.ID)))
+                if (!(context.MeasurmentUnits.Any(p => p.Id == (int)measurmentUnit.ID)))
                 {
                     context.MeasurmentUnits.Add(new MeasurmentUnit
                     {
                         Name = measurmentUnit.NAME,
-                        //Id = (int) measurmentUnit.ID
+                        Id = (int) measurmentUnit.ID
                     });
+                }
+                else
+                {
+                    var existingEntity = context.MeasurmentUnits.Find((int)measurmentUnit.ID);
+                    if (existingEntity.Name != measurmentUnit.NAME)
+                    {
+                        existingEntity.Name = measurmentUnit.NAME;
+                    }
                 }
             }
 
@@ -57,13 +65,21 @@ namespace ReplicateOracleDBIntoMSSQL.Migrations
 
             foreach (var vendor in vendors)
             {
-                if (!(context.Vendors.Any(p => p.Id == vendor.ID)))
+                if (!(context.Vendors.Any(p => p.Id == (int)vendor.ID)))
                 {
                     context.Vendors.Add(new Vendor
                     {
-                        //Id = (int) vendor.ID,
+                        Id = (int)vendor.ID,
                         Name = vendor.NAME
                     });
+                }
+                else
+                {
+                    var existingEntity = context.Vendors.Find((int)vendor.ID);
+                    if (existingEntity.Name != vendor.NAME)
+                    {
+                        existingEntity.Name = vendor.NAME;
+                    }
                 }
             }
 
@@ -79,13 +95,21 @@ namespace ReplicateOracleDBIntoMSSQL.Migrations
 
             foreach (var supermarket in supermarkets)
             {
-                if (!(context.Supermarkets.Any(p => p.Id == supermarket.ID)))
+                if (!(context.Supermarkets.Any(p => p.Id == (int)supermarket.ID)))
                 {
                     context.Supermarkets.Add(new Supermarket
                     {
-                        //Id = (int) supermarket.ID,
+                        Id = (int) supermarket.ID,
                         Name = supermarket.NAME
                     });
+                }
+                else
+                {
+                    var existingEntity = context.Supermarkets.Find((int)supermarket.ID);
+                    if (existingEntity.Name != supermarket.NAME)
+                    {
+                        existingEntity.Name = supermarket.NAME;
+                    }
                 }
             }
 
@@ -99,16 +123,30 @@ namespace ReplicateOracleDBIntoMSSQL.Migrations
 
             foreach (var product in products)
             {
-                if (!(context.Products.Any(p => p.Id == product.ID)))
+                if (!(context.Products.Any(p => p.Id == (int)product.ID)))
                 {
                     context.Products.Add(new Product
                     {
-                        //Id = (int) product.ID,
+                        Id = (int) product.ID,
                         Name = product.NAME,
                         MeasurmentUnitId = (int) product.MEASURMENT_UNIT_ID,
                         Price = product.PRICE,
                         VendorId = (int) product.VENDOR_ID
                     });
+                }
+                else
+                {
+                    var existingEntity = context.Products.Find((int)product.ID);
+                    if (existingEntity.Name != product.NAME ||
+                        existingEntity.MeasurmentUnitId != (int)product.MEASURMENT_UNIT_ID ||
+                        existingEntity.Price != product.PRICE ||
+                        existingEntity.VendorId != (int)product.VENDOR_ID)
+                    {
+                        existingEntity.Name = product.NAME;
+                        existingEntity.MeasurmentUnitId = (int) product.MEASURMENT_UNIT_ID;
+                        existingEntity.Price = product.PRICE;
+                        existingEntity.VendorId = (int) product.VENDOR_ID;
+                    }
                 }
             }
 
@@ -125,16 +163,30 @@ namespace ReplicateOracleDBIntoMSSQL.Migrations
 
             foreach (var sale in sales)
             {
-                if (!(context.Sales.Any(p => p.Id == sale.ID)))
+                if (!(context.Sales.Any(p => p.Id == (int)sale.ID)))
                 {
                     context.Sales.Add(new Sale
                     {
-                        //Id = (int) sale.ID,
+                        Id = (int) sale.ID,
                         SupermarketId = (int) sale.SUPERMARKET_ID,
                         Quantity = (int) sale.QUANTITY,
                         SoldOn = (DateTime) sale.SOLD_ON,
                         ProductId = (int) sale.PRODUCT_ID
                     });
+                }
+                else
+                {
+                    var existingEntity = context.Sales.Find((int)sale.ID);
+                    if (existingEntity.SupermarketId != (int)sale.SUPERMARKET_ID||
+                        existingEntity.Quantity !=(int) sale.QUANTITY ||
+                        existingEntity.SoldOn != (DateTime) sale.SOLD_ON ||
+                        existingEntity.ProductId != (int)sale.PRODUCT_ID)
+                    {
+                        existingEntity.SupermarketId = (int) sale.SUPERMARKET_ID;
+                        existingEntity.Quantity = (int) sale.QUANTITY;
+                        existingEntity.SoldOn = (DateTime) sale.SOLD_ON;
+                        existingEntity.ProductId = (int) sale.PRODUCT_ID;
+                    }
                 }
             }
 
