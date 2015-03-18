75837a6245619c7c0c927cd6e28d84400a373127
Nina Markova <ninamarkoff@users.noreply.github.com>
2015-03-18 13:22:13 +0200

    General exception in Oracle/SQL data migration added;

--- ReplicateOracleDBIntoMSSQL/Start.cs
+++ ReplicateOracleDBIntoMSSQL/Start.cs
@@ -9,8 +9,18 @@ namespace ReplicateOracleDBIntoMSSQL
         public static void Main()
         {
             Console.WriteLine("Start of data migration from Oracle to MS SQL, please wait :-)");
-            Migrations.Configuration.SynchronizeSQLDb(new SQLEntities());
-            Console.WriteLine("Successful data migration!");
+            try
+            {
+                Migrations.Configuration.SynchronizeSQLDb(new SQLEntities());
+                Console.WriteLine("Successful data migration!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The data could not be migrated into MS SQL." +
+                                                " Please check your connection string parameter and try again." +
+                                                " Inner Exception message: {0}", e.Message);
+            }
+            
         }
     }
 }
