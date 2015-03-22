e959d23868c64dca928cfd334218a71999f67341
milendd <milen_d@abv.bg>
2015-03-22 14:24:08 +0200

    Task 4: generate xml from sales by vendors
    In another solution

---
+++ SupermarketsChainToXML/SupermarketsChain.Data/Models.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'D:\MyDocs\Visual Studio 2012\Projects\02.SoftUni\Level3\02.DatabaseApplications\Teamwork\SupermarketsChainToXML\SupermarketsChain.Data\Models.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
---
+++ SupermarketsChainToXML/SupermarketsChain.Manager/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace SupermarketsChain.Manager
+{
+    using SupermarketsChain.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Threading;
+    using System.Xml;
+    using System.Xml.Linq;
+
+    internal class Summary
+    {
+        public Summary(DateTime date, decimal totalSum)
+        {
+            this.Date = date;
+            this.TotalSum = totalSum;
+        }
+
+        public DateTime Date { get; set; }
+
+        public decimal TotalSum { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            const string XmlResultFileName = @"..\..\..\result.xml";
+
+            var context = new SupermarketsChainEntities();
+            var startDate = new DateTime(2015, 1, 1);
+            var endDate = new DateTime(2015, 6, 30);
+
+            Console.WriteLine("Generating report from sales to xml...");
+            var sales = SalesByVendors(context, startDate, endDate);
+            GenerateXmlFromSales(sales, XmlResultFileName);
+            Console.WriteLine("The report is done!");
+        }
+
+        private static Dictionary<string, List<Summary>> SalesByVendors(SupermarketsChainEntities context, DateTime startDate, DateTime endDate)
+        {
+            using (context)
+            {
+                var salesResult = new Dictionary<string, List<Summary>>();
+                var sales = context.Sales.Select(s => new
+                {
+                    s.Product,
+                    s.Product.Vendor,
+                    s.SaledOn
+                });
+
+                foreach (var sale in sales)
+                {
+                    if (startDate <= sale.SaledOn && sale.SaledOn <= endDate)
+                    {
+                        var vendorName = sale.Product.Vendor.Name;
+                        if (!salesResult.ContainsKey(vendorName))
+                        {
+                            salesResult[vendorName] = new List<Summary>();
+                        }
+                        var summary = new Summary(sale.SaledOn, sale.Product.Price);
+                        var summariesWithEqualDates = salesResult[vendorName].Where(x => x.Date == summary.Date);
+
+                        if (summariesWithEqualDates.Count() == 0)
+                        {
+                            salesResult[vendorName].Add(summary);
+                        }
+                        else
+                        {
+                            summariesWithEqualDates.FirstOrDefault().TotalSum += summary.TotalSum;
+                        }
+                    }
+                }
+
+                return salesResult;
+            }
+        }
+
+        private static void GenerateXmlFromSales(Dictionary<string, List<Summary>> sales, string resultFileName)
+        {
+            var doc = new XDocument();
+            var xSales = new XElement("sales");
+            foreach (var sale in sales)
+            {
+                var summaries = sale.Value;
+                var xSale = new XElement("sale");
+                
+                foreach (var summary in summaries)
+                {
+                    var xSummary = new XElement("summary");
+                    xSummary.Add(new XAttribute("date", summary.Date.ToString("dd-MMM-yyyy")));
+                    xSummary.Add(new XAttribute("total-sum", string.Format("{0:F2}", summary.TotalSum)));
+                    xSale.Add(xSummary);
+                }
+
+                xSale.Add(new XAttribute("vendor", sale.Key));
+                xSales.Add(xSale);
+            }
+
+            doc.Add(xSales);
+            doc.Save(resultFileName);
+        }
+    }
+}
